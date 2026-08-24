e5fc698b1c488ebbd9795b685556e888528522b8
RYAN J EBENEZAR CSE <ryanjebenezar.cse2025@citchennai.net>
2026-08-24 08:42:59 +0530

    Add files via upload

---
+++ exp4 2.c
@@ -0,0 +1,54 @@
+//shortest job first 
+
+#include <stdio.h>
+
+int main(){
+    int n,i,j;
+    int bt[20],wt[20],tat[20];
+    float avg_wt=0;
+    float avg_tat=0;
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        printf("Enter burst time for process %d: ",i+1);
+        scanf("%d",&bt[i]);
+    }
+
+    // Sorting burst times in ascending order
+    for(i=0;i<n-1;i++){
+        for(j=i+1;j<n;j++){
+            if(bt[i]>bt[j]){
+                int temp=bt[i];
+                bt[i]=bt[j];
+                bt[j]=temp;
+            }
+        }
+    }
+
+    wt[0]=0;
+
+    for(i=1;i<n;i++){
+        wt[i]=wt[i-1]+bt[i-1];
+    }
+
+    printf("\nProcess\tBT\tWT\tTAT\n");
+
+    for(i=0;i<n;i++){
+        tat[i]=wt[i]+bt[i];
+        avg_wt+=wt[i];
+        avg_tat+=tat[i];
+
+        printf("P%d\t%d\t%d\t%d\n",i+1,bt[i],wt[i],tat[i]);
+    }
+    
+    avg_wt = avg_wt / n;
+    avg_tat = avg_tat / n;
+
+    printf("\nAverage waiting time: %.2f",avg_wt);
+    printf("\nAverage turn around time: %.2f",avg_tat);
+
+    return 0;
+}
+
