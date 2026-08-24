5b532f5148f3c7fa2b28bfc65a6d2ab0ad7dd850
RYAN J EBENEZAR CSE <ryanjebenezar.cse2025@citchennai.net>
2026-08-24 08:57:48 +0530

    Add files via upload

---
+++ exp 15.3 SCAN.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int head = 53;
+    printf("SCAN Disk Scheduling\n");
+    printf("Initial Head Position : %d\n",head);
+    printf("Head moves towards higher cylinders,\n");
+    printf("then reverses direction.\n");
+    return 0;
+}
